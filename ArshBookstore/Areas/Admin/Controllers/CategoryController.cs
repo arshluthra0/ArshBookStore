4e51bb51b1391d943c919e12eea0686aeaaf3347
arshluthra <W0777039@myscc.ca>
2021-11-16 23:26:19 -0500

    Modified the files

--- ArshBookstore/Areas/Admin/Controllers/CategoryController.cs
+++ ArshBookstore/Areas/Admin/Controllers/CategoryController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace ArshBookstore.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork unitOfWork;
