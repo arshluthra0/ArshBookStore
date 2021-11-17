f0bcdf8acb4d67ae99299d028316e511734c4e8e
arshluthra <W0777039@myscc.ca>
2021-11-16 22:55:51 -0500

    Added CategoryController.cs

---
+++ ArshBookstore/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using ArshBooks.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArshBookstore.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
+    }
+}
