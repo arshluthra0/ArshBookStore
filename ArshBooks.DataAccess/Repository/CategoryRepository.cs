dd4564120f887942edd6749c75f4c7124c1a6d03
arshluthra <W0777039@myscc.ca>
2021-11-16 21:22:33 -0500

    Modified it

--- ArshBooks.DataAccess/Repository/CategoryRepository.cs
+++ ArshBooks.DataAccess/Repository/CategoryRepository.cs
@@ -3,7 +3,7 @@ using ArshBooks.Models;
 using ArshBookstore.DataAccess.Data;
 using System;
 using System.Collections.Generic;
-//using System.Linq;
+using System.Linq;
 using System.Text;
 //using System.Threading.Tasks;
 
@@ -20,7 +20,12 @@ namespace ArshBooks.DataAccess.Repository
 
         public void Update(Category category)
         {
-            throw new NotImplementedException();
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
         }
     }
 }
