268cb079ffb895f4c751b251e45f64de62ffd86c
arshluthra <W0777039@myscc.ca>
2021-11-24 00:21:43 -0500

    added product controller

---
+++ ArshBookstore/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using ArshBooks.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArshBookstore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        private readonly IWebHostEnvironment _hostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region #region API CALLS
+        [HttpGet]
+
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Product.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
+    }
+}
