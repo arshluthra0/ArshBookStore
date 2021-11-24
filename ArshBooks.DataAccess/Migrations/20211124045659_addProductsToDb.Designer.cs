2ffdc12b133100c19bdb485598e4b12eb0f4591e
arshluthra <W0777039@myscc.ca>
2021-11-24 00:00:08 -0500

    added migrations and modified

--- ArshBooks.DataAccess/Migrations/20211124045659_addProductsToDb.Designer.cs
+++ ArshBooks.DataAccess/Migrations/20211124045659_addProductsToDb.Designer.cs
@@ -4,14 +4,16 @@ using ArshBookstore.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ArshBooks.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211124045659_addProductsToDb")]
+    partial class addProductsToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ArshBooks.DataAccess/Migrations/20211124045659_addProductsToDb.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ArshBooks.DataAccess.Migrations
+{
+    public partial class addProductsToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
